3059f3db14605553d88349a23d285e6a2d1af70f
kundzior <grzybek2413@gmail.com>
2024-03-12 23:15:00 +0100

    endpoint, uvicorn

---
+++ kanbanize/main_api/rest.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return RedirectResponse("/hello/")
+
+
+@app.get("/hello/")
+def hello():
+    return "Hello"
