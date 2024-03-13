ef2c10a8c4dff9ca0e93db64f6ed8a1be095cf68
kundzior <grzybek2413@gmail.com>
2024-03-13 14:15:14 +0100

    routers, async, exclude app to run.py

--- kanbanize/main_api/rest.py
+++ kanbanize/main_api/rest.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, FastAPI
 from fastapi.responses import RedirectResponse
 
+from kanbanize.main_api.main import app, group, table, task
 from kanbanize.main_api.schemas import (
     Group,
     GroupResponse,
@@ -10,67 +10,63 @@ from kanbanize.main_api.schemas import (
     TaskResponse,
 )
 
-table = APIRouter(prefix="/table", tags=["table"])
-task = APIRouter(prefix="/task", tags=["task"])
-group = APIRouter(prefix="/group", tags=["group"])
-
-app = FastAPI()
-app.include_router(table)
-app.include_router(task)
-app.include_router(group)
-
 
 @app.get("/")
-def read_root():
+async def read_root():
     return RedirectResponse("/docs/")
 
 
 @table.post("/create")
-def create_table(table: Table) -> TableResponse:
+async def create_table(table: Table) -> TableResponse:
     #
     return "table"
 
 
 @table.get("/get/{uuid}")
-def get_table(uuid) -> TableResponse:
+async def get_table(uuid) -> TableResponse:
     # Code to get a table goes here
     pass
 
 
 @table.patch("/edit/{uuid}")
-def edit_table(uuid) -> Table:
+async def edit_table(uuid) -> Table:
     pass
 
 
 @group.post("/create")
-def create_group(group: Group) -> GroupResponse:
+async def create_group(group: Group) -> GroupResponse:
     # Code to create a group goes here
     pass
 
 
 @group.get("/get/{uuid}")
-def get_group(uuid) -> GroupResponse:
+async def get_group(uuid) -> GroupResponse:
     # Code to get a group goes here
     return "group"
 
 
 @group.patch("/edit/{uuid}")
-def edit_group(uuid) -> GroupResponse:
+async def edit_group(uuid) -> GroupResponse:
     pass
 
 
 @task.post("/create")
-def create_task(task: Task) -> TaskResponse:
+async def create_task(task: Task) -> TaskResponse:
     # Code to create a group goes here
     pass
 
 
 @task.get("/get/{uuid}")
-def get_task(uuid) -> TaskResponse:
+async def get_task(uuid) -> TaskResponse:
     # Code to get a group goes here
     return "task"
 
 
 @task.patch("/edit/{uuid}")
-def edit_task(uuid) -> TaskResponse:
+async def edit_task(uuid) -> TaskResponse:
     pass
+
+
+app.include_router(table)
+app.include_router(task)
+app.include_router(group)
