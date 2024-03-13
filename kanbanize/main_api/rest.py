2c66bd7ff4a09e9b335c981adf4b7c88e6fb77f0
kundzior <grzybek2413@gmail.com>
2024-03-13 21:25:29 +0100

    rest examples

--- kanbanize/main_api/rest.py
+++ kanbanize/main_api/rest.py
@@ -7,6 +7,7 @@ from kanbanize.main_api.schemas import (
     TableResponse,
     Task,
     TaskResponse,
+    Uuid,
 )
 
 table = APIRouter(prefix="/table", tags=["table"])
@@ -16,18 +17,20 @@ group = APIRouter(prefix="/group", tags=["group"])
 
 @table.post("/create")
 async def create_table(table: Table) -> TableResponse:
-    #
-    return "table"
+    result = TableResponse(name=table.name, tasks=table.tasks, uuid="uuid123")
+    return result
 
 
 @table.get("/get/{uuid}")
-async def get_table(uuid) -> TableResponse:
-    # Code to get a table goes here
-    pass
+async def get_table(uuid: Uuid) -> TableResponse:
+    t1 = TaskResponse(name="test1", status="done", notes="", uuid="uuid1")
+    t2 = TaskResponse(name="test2", status="done", notes="", uuid="uuid2")
+    result = TableResponse(name="table name", tasks=[t1, t2], uuid=uuid)
+    return result
 
 
 @table.patch("/edit/{uuid}")
-async def edit_table(uuid) -> Table:
+async def edit_table(uuid: Uuid) -> Table:
     pass
 
 
@@ -38,28 +41,30 @@ async def create_group(group: Group) -> GroupResponse:
 
 
 @group.get("/get/{uuid}")
-async def get_group(uuid) -> GroupResponse:
+async def get_group(uuid: Uuid) -> GroupResponse:
     # Code to get a group goes here
     return "group"
 
 
 @group.patch("/edit/{uuid}")
-async def edit_group(uuid) -> GroupResponse:
+async def edit_group(uuid: Uuid) -> GroupResponse:
     pass
 
 
 @task.post("/create")
 async def create_task(task: Task) -> TaskResponse:
-    # Code to create a group goes here
-    pass
+    result = TaskResponse(
+        name=task.name, status=task.status, notes=task.notes, uuid="dfa21-2313"
+    )
+    return result
 
 
 @task.get("/get/{uuid}")
-async def get_task(uuid) -> TaskResponse:
-    # Code to get a group goes here
-    return "task"
+async def get_task(uuid: Uuid) -> TaskResponse:
+    result = TaskResponse(name="test", status="done", notes="", uuid=uuid)
+    return result
 
 
 @task.patch("/edit/{uuid}")
-async def edit_task(uuid) -> TaskResponse:
+async def edit_task(uuid: Uuid) -> TaskResponse:
     pass
