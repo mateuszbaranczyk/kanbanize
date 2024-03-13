0fbf0377548a9feb637dee0a944e1794d6b50cb7
kundzior <grzybek2413@gmail.com>
2024-03-13 13:37:40 +0100

    endpoint structure

--- kanbanize/main_api/rest.py
+++ kanbanize/main_api/rest.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.responses import RedirectResponse
 
 from kanbanize.main_api.schemas import (
+    Group,
     GroupResponse,
+    Table,
     TableResponse,
+    Task,
     TaskResponse,
 )
 
+table = APIRouter(prefix="/table", tags=["table"])
+task = APIRouter(prefix="/task", tags=["task"])
+group = APIRouter(prefix="/group", tags=["group"])
+
 app = FastAPI()
+app.include_router(table)
+app.include_router(task)
+app.include_router(group)
 
 
 @app.get("/")
@@ -15,37 +25,52 @@ def read_root():
     return RedirectResponse("/docs/")
 
 
-@app.post("/table")
-def create_table() -> TableResponse:
-    # Code to create a table goes here
-    pass
+@table.post("/create")
+def create_table(table: Table) -> TableResponse:
+    #
+    return "table"
 
 
-@app.get("/table")
-def get_table() -> TaskResponse:
+@table.get("/get/{uuid}")
+def get_table(uuid) -> TableResponse:
     # Code to get a table goes here
     pass
 
 
-@app.post("/group")
-def create_group() -> GroupResponse:
+@table.patch("/edit/{uuid}")
+def edit_table(uuid) -> Table:
+    pass
+
+
+@group.post("/create")
+def create_group(group: Group) -> GroupResponse:
     # Code to create a group goes here
     pass
 
 
-@app.get("/group")
-def get_group() -> GroupResponse:
+@group.get("/get/{uuid}")
+def get_group(uuid) -> GroupResponse:
     # Code to get a group goes here
+    return "group"
+
+
+@group.patch("/edit/{uuid}")
+def edit_group(uuid) -> GroupResponse:
     pass
 
 
-@app.post("/group")
-def create_task() -> TaskResponse:
+@task.post("/create")
+def create_task(task: Task) -> TaskResponse:
     # Code to create a group goes here
     pass
 
 
-@app.get("/group")
-def get_task() -> TaskResponse:
+@task.get("/get/{uuid}")
+def get_task(uuid) -> TaskResponse:
     # Code to get a group goes here
+    return "task"
+
+
+@task.patch("/edit/{uuid}")
+def edit_task(uuid) -> TaskResponse:
     pass
