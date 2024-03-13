5052fdb1002732b3056ac2395a64c4dc7af098ca
kundzior <grzybek2413@gmail.com>
2024-03-13 16:29:48 +0100

    adapter methods

--- kanbanize/main_api/adapters.py
+++ kanbanize/main_api/adapters.py
@@ -1,25 +1,44 @@
 from abc import ABC, abstractmethod
 
+from requests import request
+
+from kanbanize.main_api.schemas import Task, TaskResponse
+
 
 class IAdapter(ABC):
     address: str
-    endpoint: str
 
     @abstractmethod
-    def create(self):
-        raise NotImplementedError
+    def path(self, endpoint: str):
+        path = f"http://{self.address}/{endpoint}"
+        return path
 
     @abstractmethod
-    def get(self):
-        raise NotImplementedError
+    def create(self, endpoint, object_):
+        path = self.path(endpoint)
+        result = request.post(path, object_)
+        return result
 
     @abstractmethod
-    def edit(self):
-        raise NotImplementedError
+    def get(self, endpoint, uuid):
+        path = self.path(endpoint)
+        url = f"{path}/{uuid}/"
+        result = request.get(url)
+        return result
+
+    @abstractmethod
+    def edit(self, endpoint, uuid, object_):
+        path = self.path(endpoint)
+        url = f"{path}/{endpoint}/{uuid}"
+        result = request.patch(url, object_)
+        return result
 
 
 class TaskAdapter(IAdapter):
-    pass
+    address = "localhost"
+
+    def create(self, endpoint: str, object_: Task) -> TaskResponse:
+        return super().create(endpoint, object_)
 
 
 class GroupAdapter(IAdapter):
