fb875bbe36206dd269e200993987a7911257ee12
kundzior <grzybek2413@gmail.com>
2024-03-13 21:24:48 +0100

    new type uuid, schema logic fixes

--- kanbanize/main_api/schemas.py
+++ kanbanize/main_api/schemas.py
@@ -1,30 +1,34 @@
+from typing import NewType
+
 from pydantic import BaseModel
+
+Uuid = NewType("Uuid", str)
 
 
 class Task(BaseModel):
     name: str
     status: str
-    notes: str
+    notes: str = ""
+
+
+class TaskResponse(Task):
+    uuid: Uuid
 
 
 class Table(BaseModel):
     name: str
-    tasks: list[Task] = []
+    tasks: list[TaskResponse] = []
+
+
+class TableResponse(Table):
+    uuid: Uuid
 
 
 class Group(BaseModel):
     name: str
     project: str
-    tables: list[Table] = []
-
-
-class TaskResponse(Task):
-    uuid: str
+    tables: list[TableResponse] = []
 
 
 class GroupResponse(Group):
-    uuid: str
-
-
-class TableResponse(Table):
-    uuid: str
+    uuid: Uuid
