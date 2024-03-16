fd38429c63c48c5076c65fd8879898fa06d95ba8
kundzior <grzybek2413@gmail.com>
2024-03-16 14:22:25 +0100

    adapter full typing

--- kanbanize/main_api/adapters.py
+++ kanbanize/main_api/adapters.py
@@ -5,10 +5,14 @@ from requests import request
 from kanbanize.data_structures.schemas import (
     Group,
     GroupResponse,
+    GroupUuid,
     Table,
     TableResponse,
+    TableUuid,
     Task,
     TaskResponse,
+    TaskUuid,
+    Uuid,
 )
 
 
@@ -21,20 +25,20 @@ class IAdapter(ABC):
         return path
 
     @abstractmethod
-    def create(self, endpoint, object_):
+    def create(self, object_: dict, endpoint="create"):
         path = self.path(endpoint)
         result = request.post(path, object_)
         return result
 
     @abstractmethod
-    def get(self, endpoint, uuid):
+    def get(self, uuid: Uuid, endpoint="get"):
         path = self.path(endpoint)
         url = f"{path}/{uuid}/"
         result = request.get(url)
         return result
 
     @abstractmethod
-    def edit(self, endpoint, uuid, object_):
+    def edit(self, uuid: Uuid, object_: dict, endpoint="edit"):
         path = self.path(endpoint)
         url = f"{path}/{endpoint}/{uuid}"
         result = request.patch(url, object_)
@@ -44,37 +48,37 @@ class IAdapter(ABC):
 class TaskAdapter(IAdapter):
     location = "localhost"
 
-    def create(self, endpoint: str, object_: Task) -> TaskResponse:
-        return super().create(endpoint, object_)
+    def create(self, object_: Task.dict) -> TaskResponse:
+        return super().create(object_)
 
-    def get(self, endpoint: str, uuid: str) -> TaskResponse:
-        return super().get(endpoint, uuid)
+    def get(self, uuid: TaskUuid) -> TaskResponse:
+        return super().get(uuid)
 
-    def edit(self, endpoint: str, uuid: str, object_: Task) -> TaskResponse:
-        return super().edit(endpoint, uuid, object_)
+    def edit(self, uuid: TaskUuid, object_: Task.dict) -> TaskResponse:
+        return super().edit(uuid, object_)
 
 
 class GroupAdapter(IAdapter):
     location = "localhost"
 
-    def create(self, endpoint: str, object_: Group) -> GroupResponse:
-        return super().create(endpoint, object_)
+    def create(self, object_: Group.dict) -> GroupResponse:
+        return super().create(object_)
 
-    def get(self, endpoint: str, uuid: str) -> GroupResponse:
-        return super().get(endpoint, uuid)
+    def get(self, uuid: GroupUuid) -> GroupResponse:
+        return super().get(uuid)
 
-    def edit(self, endpoint: str, uuid: str, object_: Group) -> GroupResponse:
-        return super().edit(endpoint, uuid, object_)
+    def edit(self, uuid: GroupUuid, object_: Group.dict) -> GroupResponse:
+        return super().edit(uuid, object_)
 
 
 class TableAdapter(IAdapter):
     location = "localhost"
 
-    def create(self, endpoint: str, object_: Table) -> TableResponse:
-        return super().create(endpoint, object_)
+    def create(self, object_: Table.dict) -> TableResponse:
+        return super().create(object_)
 
-    def get(self, endpoint: str, uuid: str) -> TableResponse:
-        return super().get(endpoint, uuid)
+    def get(self, uuid: TableUuid) -> TableResponse:
+        return super().get(uuid)
 
-    def edit(self, endpoint: str, uuid: str, object_: Table) -> TableResponse:
-        return super().edit(endpoint, uuid, object_)
+    def edit(self, uuid: TableUuid, object_: Table.dict) -> TableResponse:
+        return super().edit(uuid, object_)
