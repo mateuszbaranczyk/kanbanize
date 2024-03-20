ce289d612402f5fb55c0ec0ff0073eb80a26603a
kundzior <grzybek2413@gmail.com>
2024-03-20 20:26:15 +0100

    get task

--- kanbanize/tasks/crud.py
+++ kanbanize/tasks/crud.py
@@ -1,6 +1,6 @@
 from google.cloud import firestore
 
-from kanbanize.data_structures.schemas import Task, TaskResponse
+from kanbanize.data_structures.schemas import Task, TaskResponse, TaskUuid
 from kanbanize.tasks.database import DB_TIMEOUT, TASKS_COLLECTION
 
 
@@ -11,6 +11,15 @@ def create_task(db: firestore.Client, task: Task) -> TaskResponse:
     db_document = db.collection(TASKS_COLLECTION).document(db_object.uuid)
     db_document.set(data=task_dump, timeout=DB_TIMEOUT)
     return db_object
+
+
+def get_task(db: firestore.Client, uuid: TaskUuid) -> TaskResponse:
+    db_document = db.collection(TASKS_COLLECTION).document(uuid)
+    task = db_document.get()
+    if task.exists:
+        return TaskResponse(**task._doc)
+    else:
+        raise NameError(f"No such document! {uuid}")
 
 
 # https://cloud.google.com/firestore/docs/manage-data/add-data
