d48caff6e184ba104a19452695e3158b266db568
kundzior <grzybek2413@gmail.com>
2024-03-20 20:59:47 +0100

    test rest get

--- tests/tasks_worker/test_rest.py
+++ tests/tasks_worker/test_rest.py
@@ -1,3 +1,5 @@
+from conftest import create_new_task
+
 from kanbanize.data_structures.schemas import TASK_PREFIX, TaskResponse
 
 
@@ -12,3 +14,12 @@ def test_create_task(client, task):
     assert task_response.notes == task.notes
     assert task_response.table_uuid == ""
     assert task_response.uuid.startswith(TASK_PREFIX)
+
+
+def test_get_task(client, mock_db, task):
+    task = create_new_task(mock_db, task)
+    response = client.get(f"/task/get/{task.uuid}")
+    task_response = TaskResponse(**response.json())
+
+    assert response.status_code == 200
+    assert task_response.uuid == task.uuid
