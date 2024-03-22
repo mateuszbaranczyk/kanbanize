9507c22a70e45e32f30c3fb28c84d100091446a2
kundzior <grzybek2413@gmail.com>
2024-03-22 19:31:39 +0100

    fakedb

--- tests/tasks_worker/conftest.py
+++ tests/tasks_worker/conftest.py
@@ -7,6 +7,25 @@ from kanbanize.tasks.database import get_db
 from kanbanize.tasks.rest import app
 
 
+class FakeFirestore(MockFirestore):
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if FakeFirestore._instance is None:
+            FakeFirestore()
+        return FakeFirestore._instance
+
+    def __init__(self):
+        if FakeFirestore._instance is not None:
+            raise Exception(
+                "Instance already created. Use get_instance method."
+            )
+        else:
+            super().__init__()
+            FakeFirestore._instance = self
+
+
 @fixture
 def task() -> Task:
     task = Task(name="task_name", status="done", notes="test notes")
@@ -24,25 +43,24 @@ def client() -> TestClient:
     return client
 
 
-def mocked_db() -> MockFirestore:
-    mock_db = MockFirestore()
-    try:
-        yield mock_db
-    finally:
-        mock_db.reset()
-
-
 @fixture(autouse=True)
 def override_get_db() -> None:
-    app.dependency_overrides[get_db] = mocked_db
+    app.dependency_overrides[get_db] = FakeFirestore.get_instance
     return None
 
 
 @fixture
 def mock_db():
-    db = MockFirestore()
-    yield db
-    db.reset()
+    db = FakeFirestore.get_instance()
+    return db
+
+
+@fixture(autouse=True)
+def setup_teardown(mock_db):
+    try:
+        mock_db
+    finally:
+        mock_db.reset()
 
 
 def create_new_task(mock_db, task) -> TaskResponse:
