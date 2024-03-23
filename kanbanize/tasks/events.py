10e7511b0f95e11bf38e127a98aca73f45cc25bd
kundzior <grzybek2413@gmail.com>
2024-03-23 21:22:19 +0100

    created message sending

--- kanbanize/tasks/events.py
+++ kanbanize/tasks/events.py
@@ -1,12 +1,29 @@
-# TODO
-# send message to queue
-# save event in db?
+import os
+
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+
+from kanbanize.data_structures import Task
+
+HOST = os.getenv("RMQ_HOST", "localhost")
+QUEUE = "tasks"
+EXCHANGE = ""
 
 
-def send_message(task, table_uuid):
+def create_chanel() -> BlockingChannel:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE)
+
+
+def create_body_message(task: Task) -> str:
+    return f"::TASK connected to TABLE:: {task.uuid} -> {task.table_uuid}"
+
+
+def send_message(task: Task, table_uuid: str | None) -> None:
     if table_uuid:
-        send_event(task)
-
-
-def send_event(task):
-    pass
+        channel = create_chanel()
+        body = create_body_message(task)
+        channel.basic_publish(
+            exchange=EXCHANGE, routing_key="hello", body=body
+        )
