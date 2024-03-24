c408c3938ca883820a5940e41cd11a46a79a7daa
kundzior <grzybek2413@gmail.com>
2024-03-24 15:43:37 +0100

    early listener

---
+++ kanbanize/tasks/listener.py
@@ -0,0 +1,21 @@
+import time
+
+import pika
+
+from kanbanize.tasks.events import HOST, QUEUE
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+channel = connection.channel()
+
+channel.queue_declare(queue=QUEUE, durable=True)
+
+
+def callback(ch, method, properties, body):
+    time.sleep(body.count(b"."))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue=QUEUE, on_message_callback=callback)
+
+channel.start_consuming()
