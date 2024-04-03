18b013f37516db422aa54aff3287d0241111f61d
kundzior <grzybek2413@gmail.com>
2024-04-03 14:52:36 +0200

    refactor

--- kanbanize/tasks/events.py
+++ kanbanize/tasks/events.py
@@ -2,7 +2,7 @@ import os
 from abc import ABC, abstractmethod
 
 from kanbanize.rabbit_sender import RmqSender
-from kanbanize.schemas import TABLE_PREFIX, Task
+from kanbanize.schemas import TABLE_PREFIX, TaskResponse
 
 
 class TaskEvent(RmqSender, ABC):
@@ -11,39 +11,42 @@ class TaskEvent(RmqSender, ABC):
     exchange = os.getenv("EXCHANGE", "")
     routing_key = os.getenv("ROUTING_KEY", "tasks")
 
-    def __init__(self, task: Task) -> None:
+    def __init__(self, task: TaskResponse) -> None:
         super().__init__()
         self.task = task
 
     def send(self) -> None:
-        body = self.create_body_message(self.task)
+        body = self.create_body_message()
         self.send_message(body)
 
     @abstractmethod
-    def create_body_message(self, task: Task) -> str:
+    def create_body_message(self) -> str:
         raise NotImplementedError
 
 
 class TaskConnectedEvent(TaskEvent):
-    def __init__(self, task: Task) -> None:
+    def __init__(self, task: TaskResponse) -> None:
         super().__init__(task)
 
-    def create_body_message(self, task: Task) -> str:
-        return f"::TASK connected to TABLE:: {task.uuid} -> {task.table_uuid}"
-
-
-class TaskDisconnectedEvent(TaskEvent):
-    def __init__(self, task: Task) -> None:
-        super().__init__(task)
-
-    def create_body_message(self, task: Task) -> str:
+    def create_body_message(self) -> str:
         return (
-            f"::TASK disconnected from TABLE:: {task.uuid} "
-            f"from {task.table_uuid}"
+            f"::TASK connected to TABLE:: "
+            f"{self.task.uuid} -> {self.task.table_uuid}"
         )
 
 
-def handle_events(data: dict, task: Task) -> None:
+class TaskDisconnectedEvent(TaskEvent):
+    def __init__(self, task: TaskResponse) -> None:
+        super().__init__(task)
+
+    def create_body_message(self) -> str:
+        return (
+            f"::TASK disconnected from TABLE:: {self.task.uuid} "
+            f"from {self.task.table_uuid}"
+        )
+
+
+def handle_events(data: dict, task: TaskResponse) -> None:
     table_uuid = data.get("table_uuid", "No table uuid")
     if table_uuid.startswith(TABLE_PREFIX):
         TaskConnectedEvent(task).send()
