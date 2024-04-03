84573df93c613a7b1f27a1b88410ab8c4754bf38
kundzior <grzybek2413@gmail.com>
2024-04-03 19:09:50 +0200

    task adapter implementation

--- kanbanize/main_api/rest.py
+++ kanbanize/main_api/rest.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from kanbanize.main_api.adapters import TaskAdapter
 from kanbanize.schemas import (
@@ -49,17 +49,24 @@ async def edit_group(uuid: GroupUuid, group: Group) -> GroupResponse:
 
 
 @task.post("/create")
-async def create_task(task: Task) -> TaskResponse:
-    adapter = TaskAdapter()
-    task_response = adapter.create(task.model_dump_json())
-    return task_response
+async def create_task(
+    task: Task, adapter: TaskAdapter = Depends(TaskAdapter)
+) -> TaskResponse:
+    task_data = task.model_dump_json()
+    return adapter.create(task_data)
 
 
 @task.get("/get/{uuid}")
-async def get_task(uuid: TaskUuid) -> TaskResponse:
-    return uuid
+async def get_task(
+    uuid: TaskUuid, adapter: TaskAdapter = Depends(TaskAdapter)
+) -> TaskResponse:
+    return adapter.get(uuid)
 
 
 @task.put("/edit/{uuid}")
-async def edit_task(uuid: TaskUuid, task: Task) -> TaskResponse:
-    return task
+async def edit_task(
+    uuid: TaskUuid,
+    task_data: dict,
+    adapter: TaskAdapter = Depends(TaskAdapter),
+) -> TaskResponse:
+    return adapter.edit(uuid, task_data)
