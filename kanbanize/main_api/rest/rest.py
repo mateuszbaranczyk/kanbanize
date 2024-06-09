ac3c5f1721751915ceaaa33596244d2a9cb7d3ae
kundzior <grzybek2413@gmail.com>
2024-06-09 19:34:48 +0200

    moved task rest to file

--- kanbanize/main_api/rest/rest.py
+++ kanbanize/main_api/rest/rest.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
-from kanbanize.main_api.adapters import TaskAdapter
 from kanbanize.schemas import (
     Group,
     GroupResponse,
@@ -8,12 +7,8 @@ from kanbanize.schemas import (
     Table,
     TableResponse,
     TableUuid,
-    Task,
-    TaskResponse,
-    TaskUuid,
 )
 
-task = APIRouter(prefix="/task", tags=["task"])
 table = APIRouter(prefix="/table", tags=["table"])
 group = APIRouter(prefix="/group", tags=["group"])
 
@@ -48,25 +43,4 @@ async def edit_group(uuid: GroupUuid, group: Group) -> GroupResponse:
     return group
 
 
-@task.post("/create")
-async def create_task(
-    task: Task, adapter: TaskAdapter = Depends(TaskAdapter)
-) -> TaskResponse:
-    task_data = task.model_dump_json()
-    return adapter.create(task_data)
 
-
-@task.get("/get/{uuid}")
-async def get_task(
-    uuid: TaskUuid, adapter: TaskAdapter = Depends(TaskAdapter)
-) -> TaskResponse:
-    return adapter.get(uuid)
-
-
-@task.put("/edit/{uuid}")
-async def edit_task(
-    uuid: TaskUuid,
-    task_data: dict,
-    adapter: TaskAdapter = Depends(TaskAdapter),
-) -> TaskResponse:
-    return adapter.edit(uuid, task_data)
