868dd1dc33130930ee4391025df12bbaa6dcce90
kundzior <grzybek2413@gmail.com>
2024-06-11 16:40:11 +0200

    new rest implementation

--- kanbanize/tasks/rest.py
+++ kanbanize/tasks/rest.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter, Depends, FastAPI, HTTPException
-from google.cloud import firestore
+from fastapi import APIRouter, FastAPI, HTTPException
 
+from kanbanize.firestore_adapter import DocumentError
 from kanbanize.schemas import Task, TaskResponse, TaskUuid
 from kanbanize.tasks import crud
-from kanbanize.tasks.database import get_db
 from kanbanize.tasks.events import (  # noqa: F401
     TaskConnectedEvent,
     TaskDisconnectedEvent,
@@ -17,36 +16,37 @@ task = APIRouter(prefix="/task")
 
 
 @task.post("/create")
-async def create(
-    task: Task, db: firestore.Client = Depends(get_db)
-) -> TaskResponse:
-    result = crud.create(db, task)
+async def create(task: Task) -> TaskResponse:
+    adapter = crud.TasksAdapter()
+    try:
+        result = adapter.create(data=task)
+    except DocumentError:
+        raise HTTPException(500)
+
     if task.table_uuid:
         TaskConnectedEvent(result).send()
     return result
 
 
 @task.get("/get/{uuid}")
-async def get(
-    uuid: TaskUuid, db: firestore.Client = Depends(get_db)
-) -> TaskResponse:
+async def get(uuid: TaskUuid) -> TaskResponse:
+    adapter = crud.TasksAdapter()
     try:
-        task = crud.get(db, uuid)
+        task = adapter.get(uuid)
     except NameError:
         raise HTTPException(404)
     return task
 
 
 @task.put("/edit/{uuid}")
-async def edit(
-    data: dict, uuid: TaskUuid, db: firestore.Client = Depends(get_db)
-) -> TaskResponse:
+async def edit(data: dict, uuid: TaskUuid) -> TaskResponse:
     validate(data)
+    adapter = crud.TasksAdapter()
     try:
-        task = crud.edit(db, uuid, data)
+        result = adapter.edit(uuid, data)
     except NameError:
         raise HTTPException(404)
-    handle_events(data, task)
+    handle_events(data, result)
     return task
 
 
