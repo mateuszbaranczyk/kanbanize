3e6cdaa6f192dd1208d353e5d987137c98fea53a
kundzior <grzybek2413@gmail.com>
2024-06-11 16:41:59 +0200

    remove unused crud

--- kanbanize/tasks/crud.py
+++ kanbanize/tasks/crud.py
@@ -1,5 +1,3 @@
-from google.cloud import firestore
-
 from kanbanize.firestore_adapter import FirestoreAdapter
 from kanbanize.schemas import Task, TaskResponse, TaskUuid
 from kanbanize.tasks import database
@@ -19,35 +17,3 @@ class TasksAdapter(FirestoreAdapter):
 
     def edit(self, uuid: TaskUuid, data: dict) -> TaskResponse:
         return super().edit(uuid, data)
-
-
-def create(db: firestore.Client, task: Task) -> TaskResponse:
-    db_object = TaskResponse(**task.model_dump())
-    task_dump = db_object.model_dump()
-
-    created_document = _save_and_get(db, db_object, task_dump)
-    return TaskResponse(**created_document)
-
-
-def _save_and_get(db, db_object: TaskResponse, task_dump: dict) -> dict:
-    db_document = db.collection(database.COLLECTION).document(db_object.uuid)
-    db_document.set(task_dump, timeout=database.DB_TIMEOUT)
-    created_document = (
-        db.collection(database.COLLECTION).document(db_object.uuid).get()
-    ).to_dict()
-
-    return created_document
-
-
-def edit(db: firestore.Client, uuid: TaskUuid, data: dict) -> TaskResponse:
-    pass
-
-
-def _edit_and_get(db, uuid: TaskUuid, task_data: dict) -> dict:
-    db_document = db.collection(database.COLLECTION).document(uuid)
-    db_document.update(task_data, timeout=database.DB_TIMEOUT)
-    edited_document = (
-        db.collection(database.COLLECTION).document(uuid).get()
-    ).to_dict()
-
-    return edited_document
