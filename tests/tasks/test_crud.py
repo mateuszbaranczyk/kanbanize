3911874d8eb1531a3c4d5ca4cf57e14b3d69e9a4
kundzior <grzybek2413@gmail.com>
2024-06-11 18:01:31 +0200

    tests refactor

--- tests/tasks/test_crud.py
+++ tests/tasks/test_crud.py
@@ -1,36 +1,38 @@
-from conftest import create_new_task
-from pytest import raises
+from pytest import fixture, raises
 
 from kanbanize.schemas import TASK_PREFIX, TaskResponse
-from kanbanize.tasks.crud import create, edit, get
+from kanbanize.tasks.crud import TasksAdapter
 
 
-def test_create_task(mock_db, task):
-    result = create(mock_db, task)
+@fixture
+def task_adapter(mock_db):
+    return TasksAdapter(mock_db)
+
+
+def test_create_task(task_adapter, task):
+    result = task_adapter.create(task)
     assert isinstance(result, TaskResponse)
     assert result.uuid.startswith(TASK_PREFIX)
 
 
-def test_get_task(mock_db, task):
-    new_task = create_new_task(mock_db, task)
-    result = get(mock_db, new_task.uuid)
-
+def test_get_task(task_adapter, task):
+    new_task = task_adapter.create(task)
+    result = task_adapter.get(new_task.uuid)
     assert isinstance(result, TaskResponse)
     assert result.uuid == new_task.uuid
 
 
-def test_get_task_name_error(mock_db):
+def test_get_task_name_error(task_adapter):
     uuid = "ta-test"
     with raises(NameError, match=f"No such document! {uuid}"):
-        get(mock_db, uuid)
+        task_adapter.get(uuid)
 
 
-def test_edit_task(mock_db, task):
-    task = create_new_task(mock_db, task)
+def test_edit_task(task_adapter, task):
+    task = task_adapter.create(task)
     new_name = "new name"
     data = {"name": new_name}
 
-    result = edit(mock_db, task.uuid, data)
-
+    result = task_adapter.edit(task.uuid, data)
     assert result.name == new_name
     assert result.notes == task.notes
