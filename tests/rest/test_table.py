01fe5b9311e5b0957e19a5f5b9bef3335d144293
kundzior <grzybek2413@gmail.com>
2024-06-11 18:00:37 +0200

    skip future tests

--- tests/rest/test_table.py
+++ tests/rest/test_table.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from kanbanize.main_api.run import rest
@@ -5,17 +6,20 @@ from kanbanize.main_api.run import rest
 client = TestClient(rest)
 
 
+@pytest.mark.skip()
 def test_create_table():
     response = client.post("/table/create", json={"name": "test table"})
     assert response.status_code == 200
     assert response.json()["name"] == "test table"
 
 
+@pytest.mark.skip()
 def test_get_table():
     response = client.get("/table/get/1")
     assert response.status_code == 200
 
 
+@pytest.mark.skip()
 def test_edit_table():
     response = client.put("/table/edit/1", json={"name": "edited table"})
     assert response.status_code == 200
