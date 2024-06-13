80d1833add1c204e9104bf832280f394472f1fb6
kundzior <grzybek2413@gmail.com>
2024-06-13 19:03:00 +0200

    table create test fix

--- tests/tables/test_rest_tables.py
+++ tests/tables/test_rest_tables.py
@@ -3,7 +3,8 @@ from kanbanize.schemas import TABLE_PREFIX, TableResponse
 
 def test_create_table(client, table):
     table_dump = table.model_dump_json()
-    response = client.post("/table/create/", data=table_dump)
+    response = client.post("/tables/create", data=table_dump)
+    assert response.status_code == 200
     table_response = TableResponse(**response.json())
 
     assert response.status_code == 200
