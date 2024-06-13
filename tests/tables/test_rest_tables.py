662bb41af643072c485dd5fe42679a9fea6e2c22
kundzior <grzybek2413@gmail.com>
2024-06-13 19:28:25 +0200

    test get table

--- tests/tables/test_rest_tables.py
+++ tests/tables/test_rest_tables.py
@@ -4,9 +4,24 @@ from kanbanize.schemas import TABLE_PREFIX, TableResponse
 def test_create_table(client, table):
     table_dump = table.model_dump_json()
     response = client.post("/tables/create", data=table_dump)
-    assert response.status_code == 200
     table_response = TableResponse(**response.json())
 
     assert response.status_code == 200
     assert table_response.name == table.name
     assert table_response.uuid.startswith(TABLE_PREFIX)
+
+
+def test_get_table(client, table, mock_db):
+    table_uuid = create_new_table(mock_db=mock_db, table=table).uuid
+
+    response = client.get(f"/tables/get/{table_uuid}")
+
+    assert response.status_code == 200
+
+
+def create_new_table(mock_db, table) -> TableResponse:
+    new_table = TableResponse(**table.model_dump())
+    mock_db.collection("tables").document(new_table.uuid).set(
+        new_table.model_dump()
+    )
+    return new_table
