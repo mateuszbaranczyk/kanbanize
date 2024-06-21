1e601d43c9bde2af31d98041ceca385d09daecfc
kundzior <grzybek2413@gmail.com>
2024-06-21 21:46:20 +0200

    listener

--- kanbanize/tables/listener.py
+++ kanbanize/tables/listener.py
@@ -1,19 +1,33 @@
-import time
+import os
 
 import pika
 
-HOST = ""
-QUEUE = ""
+cred = pika.credentials.PlainCredentials(
+    username="listener", password="listener"
+)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+HOST = os.getenv("RMQ_HOST", "localhost")
+QUEUE = os.getenv("QUEUE", "tasks")
+EXCHANGE = os.getenv("EXCHANGE", "")
+ROUTING_KEY = os.getenv("ROUTING_KEY", "tasks")
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=HOST, port=5672, credentials=cred)
+)
 channel = connection.channel()
 
 channel.queue_declare(queue=QUEUE, durable=True)
 
 
-def callback(ch, method, properties, body):
-    time.sleep(body.count(b"."))
+def callback(ch, method, properties, body: bytes):
+    print(f" [x] Received {body}")
+    msg = str(body)
+    table_uuid_pos = msg.find(" tb-")
+    task_uuid_pos = msg.find(" ta-")
+    table_uuid = msg[table_uuid_pos : table_uuid_pos + 39]  # noqa: E203
+    task_uuid = msg[task_uuid_pos : task_uuid_pos + 39]  # noqa: E203
     ch.basic_ack(delivery_tag=method.delivery_tag)
+    print("Consumed", table_uuid, task_uuid)
 
 
 channel.basic_qos(prefetch_count=1)
