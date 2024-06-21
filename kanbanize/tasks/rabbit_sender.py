e4cdaae3fb0d25e5a2d433788974e7ff1a79aa98
kundzior <grzybek2413@gmail.com>
2024-06-21 21:47:36 +0200

    rabbit sender host

--- kanbanize/tasks/rabbit_sender.py
+++ kanbanize/tasks/rabbit_sender.py
@@ -1,5 +1,9 @@
 import pika
 
+cred = pika.credentials.PlainCredentials(
+    username="listener", password="listener"
+)  # TODO
+
 
 class RmqSender:
     host: str
@@ -9,7 +13,9 @@ class RmqSender:
 
     def __init__(self) -> None:
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.host)
+            pika.ConnectionParameters(
+                host="raspberry", port=5672, credentials=cred
+            )
         )
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=self.queue, durable=True)
