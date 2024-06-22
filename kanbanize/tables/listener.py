69ec6c656a57f6248ef81d163c2dd0a4d93d660f
kundzior <grzybek2413@gmail.com>
2024-06-22 09:47:27 +0200

    parsing mesage

--- kanbanize/tables/listener.py
+++ kanbanize/tables/listener.py
@@ -1,6 +1,9 @@
+import logging
 import os
 
 import pika
+
+logger = logging.getLogger()
 
 cred = pika.credentials.PlainCredentials(
     username="listener", password="listener"
@@ -19,18 +22,28 @@ channel = connection.channel()
 channel.queue_declare(queue=QUEUE, durable=True)
 
 
-def callback(ch, method, properties, body: bytes):
-    print(f" [x] Received {body}")
-    msg = str(body)
+def parse_message(msg: str):
     table_uuid_pos = msg.find(" tb-")
     task_uuid_pos = msg.find(" ta-")
     table_uuid = msg[table_uuid_pos : table_uuid_pos + 39]  # noqa: E203
     task_uuid = msg[task_uuid_pos : task_uuid_pos + 39]  # noqa: E203
+    return table_uuid, task_uuid
+
+
+def callback(ch, method, properties, body: bytes):
+    msg = str(body)
+    logger.info(msg)
+    table_uuid, task_uuid = parse_message(msg)
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    print("Consumed", table_uuid, task_uuid)
+    logger.info("Consumed", table_uuid, task_uuid)
 
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue=QUEUE, on_message_callback=callback)
 
 channel.start_consuming()
+
+
+class RabbitWorker:
+    def __init__(self):
+        pass
